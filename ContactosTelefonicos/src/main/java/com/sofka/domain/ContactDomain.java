200b17d10de8b7afbcbdf149fcdc14ae6b568e6a
LilianaGallego <lilianagallegom@gmail.com>
2022-07-09 14:47:31 -0500

    se modifica la clase ContactDomain se agrega la relacion de uno a muchos con la clase PhoneDomain que se crea

--- ContactosTelefonicos/src/main/java/com/sofka/domain/ContactDomain.java
+++ ContactosTelefonicos/src/main/java/com/sofka/domain/ContactDomain.java
@@ -1,37 +1,73 @@
 package com.sofka.domain;
-
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Table;
 import javax.persistence.Id;
-import java.io.Serializable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-
+/**
+ * Entidad del Contacto
+ *
+ * @version 1.0.0 2022-07-08
+ * @author Martha Liliana Gallego<lilianagallegom@gmail.com>
+ * @since 1.0.0
+ */
 @Data
 @Entity
 @Table(name = "contact")
-public class ContactDomain implements Serializable{
+public class ContactDomain {
 
+    /**
+     * Variable usada para manejar el tema del identificador de la tupla (consecutivo)
+     */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * Identificador de la tupla
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "con_id")
-    private Long id;
+    @Column(name = "con_id", nullable = false)
+    private Integer id;
 
-    @Column(name = "con_full_name")
+    /**
+     * Nombre completo del contacto
+     */
+    @Column(name = "con_full_name", nullable = false, length = 45)
     private String fullName;
 
-    @Column(name = "con_phone")
-    private String phone;
-
-    @Column(name = "con_email")
+    /**
+     * Correo electronico del contacto
+     */
+    @Column(name = "con_email", nullable = false, length = 45)
     private String email;
 
-    @Column(name = "con_birthday")
+    /**
+     * Fecha de nacimiento del contacto
+     */
+    @Column(name = "con_birthday", nullable = false)
     private String birthday;
+
+    /**
+     * Punto de enlace entre la entidad del Contacto y Teléfono (un contacto puede tener muchos números de teléfono)
+     */
+    @OneToMany(
+            fetch = FetchType.EAGER,
+            targetEntity = PhoneDomain.class,
+            cascade = CascadeType.REMOVE,
+            mappedBy = "contact"
+    )
+    @JsonManagedReference
+    private List<PhoneDomain> phones = new ArrayList<>();
+
 }
--- ContactosTelefonicos/src/main/java/com/sofka/domain/PhoneDomain.java
+++ ContactosTelefonicos/src/main/java/com/sofka/domain/PhoneDomain.java
@@ -1,2 +1,56 @@
-package com.sofka.domain;public class PhoneDomain {
+package com.sofka.domain;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Entidad del Telefono
+ *
+ * @version 1.0.0 2022-07-08
+ * @author Martha Liliana Gallego<lilianagallegom@gmail.com>
+ * @since 1.0.0
+ */
+@Data
+@Entity
+@Table(name = "phone")
+public class PhoneDomain {
+    /**
+     * Variable usada para manejar el tema del identificador de la tupla (consecutivo)
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Identificador de la tupla
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pho_id", nullable = false)
+    private Integer id;
+
+    /**
+     * Punto de enlace con la entidad Contacto (un contacto puede tener muchos números de teléfono)
+     */
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = ContactDomain.class, optional = false)
+    @JoinColumn(name = "pho_contact_id", nullable = false)
+    @JsonBackReference
+    private ContactDomain contact;
+
+    /**
+     * Número de teléfono
+     */
+    @Column(name = "pho_number", nullable = false, length = 15)
+    private String number;
+
+
+
 }
